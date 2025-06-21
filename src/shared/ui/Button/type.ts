15532a2bf480dd5c5431e05cfef31f0304c4b270
Andrey Krivosheev <andreykrt12@gmail.com>
2025-06-21 23:19:39 +0300

    New

--- src/shared/ui/Button/type.ts
+++
@@ -1,7 +0,0 @@
-export type TButtonUIProps = {
-	onClick?: () => void;
-	color: 'primary' | 'secondary' | 'tertiary';
-	size?: 'small' | 'medium' | 'large';
-	text: string;
-	isDisabled?: boolean;
-};
