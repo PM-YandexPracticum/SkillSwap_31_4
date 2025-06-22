81aa00897f9fdcdbcbdadf32287259ce29f59f7a
daydreamer <nimandrey22@gmail.com>
2025-06-22 10:43:48 +0300

    fix: adjust button size

--- src/components/ui/app-header/AppHeader.tsx
+++ src/components/ui/app-header/AppHeader.tsx
@@ -1,10 +1,8 @@
 import { Logo } from '@ui';
 import styles from './AppHeader.module.scss';
 
-export const AppHeaderUI = () => {
-	return (
-		<div className={styles.header}>
-			<Logo />
-		</div>
-	);
-};
+export const AppHeaderUI = () => (
+	<div className={styles.header}>
+		<Logo />
+	</div>
+);
