5386d96a9094fd64b8eb0f31f47b91d2a8f1f09d
Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>
2025-06-22 14:42:32 +0500

    ToogleSwitch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,5 @@
 import './App.css';
-import { AppHeader } from '@components';
 
-<<<<<<< HEAD
 const App = () => <div />;
-=======
-function App() {
-	return <AppHeader />;
-}
->>>>>>> 071a26a8d361ed4669dc742955c15f926ad5182d
 
 export default App;
