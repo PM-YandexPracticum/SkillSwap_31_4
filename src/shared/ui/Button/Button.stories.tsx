28a5066ea19e10158c527de11eef6d8d789eb0d1
Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>
2025-06-22 14:42:32 +0500

    ToogleSwitch

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,30 +1,30 @@
-import type { Meta, StoryObj } from "@storybook/react-vite";
-import { ButtonUI } from "./Button";
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { ButtonUI } from './Button';
 
 const meta = {
-  title: 'Shared/UI/Button',
-  component: ButtonUI,
-  tags: ['autodocs'],
-  parameters: {
-    layout: 'centered'
-  }
+	title: 'Shared/UI/Button',
+	component: ButtonUI,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
 } satisfies Meta<typeof ButtonUI>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const PrimaryButton: Story = {
-  args: {
-    onClick: () => alert('Primary Button Clicked'),
-    color: 'primary',
-    width: '200px',
-    text: 'Primary',
-    isDisabled: false
-  },
+	args: {
+		onClick: () => alert('Primary Button Clicked'),
+		color: 'primary',
+		width: '200px',
+		text: 'Primary',
+		isDisabled: false,
+	},
 
-  render: (args) => (
-    <div style={{width: '400px', display: 'flex', justifyContent: 'center'}}>
-      <ButtonUI {...args} />
-    </div>
-  )
-}
+	render: (args) => (
+		<div style={{ width: '400px', display: 'flex', justifyContent: 'center' }}>
+			<ButtonUI {...args} />
+		</div>
+	),
+};
