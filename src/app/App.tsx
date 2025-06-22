7f780ec37600d6481d4a2064dcffa60072bc0716
Andrew Nim <131737840+daydreamonly@users.noreply.github.com>
2025-06-22 10:57:28 +0300

    feat: add UI Button component
    * feat: add Button component
    
    * fix: update colors in styles
    
    * fix: index.css rename index.scss
    
    * fix: preview deleted index.css import
    
    * fix: adjust button size

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
 import './App.css';
 
+<<<<<<< HEAD
 const App = () => <div />;
+=======
+function App() {
+	return <AppHeader />;
+}
+>>>>>>> 071a26a8d361ed4669dc742955c15f926ad5182d
 
 export default App;
