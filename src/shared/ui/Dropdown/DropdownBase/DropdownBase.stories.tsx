13a699f48157a11909c602beccf50bfc2b686731
Mkkkash <143110196+Katrinmark2000@users.noreply.github.com>
2025-06-24 12:18:10 +0300

    fix/DropdownBase
    * Ready Dropdown
    
    * fix:create dropdownBase and dropdownSingle
    
    * fix styles
    
    * add dropdownMulti
    
    * feat: add UI Button component 
    
    * feat: add Button component
    
    * fix: update colors in styles
    
    * fix: index.css rename index.scss
    
    * fix: preview deleted index.css import
    
    * fix: adjust button size
    
    * ToogleSwitch
    
    * feature: SkillTags
    
    Co-authored-by: Mikhail Zhilkin <mikhailzhilkin@yandex.ru>
    
    * feature/like button
    
    * feat: add Button component
    
    * fix: update colors in styles
    
    * fix: index.css rename index.scss
    
    * feat: add UI LikeButton component
    
    * feat: refactor LikeButtonUI component to accept props for state management
    
    * feat: update LikeButton component to handle click events and improve styling
    
    ---------
    
    Co-authored-by: Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>
    
    * Fix/like button
    
    * fix: update LikeButton component props to be optional and add HeaderLikeButton story
    
    * feature/visible-button
    
    * Add Visibility Button
    
    * Fix Visibility Button
    
    * Fix-2 Visibility Button
    
    ---------
    
    Co-authored-by: Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>
    
    * add new droopdownBase
    
    * create dropdownBase
    
    * Ready Dropdown
    
    * feat: add UI Button component
    
    * feat: add Button component
    
    * fix: update colors in styles
    
    * fix: index.css rename index.scss
    
    * fix: preview deleted index.css import
    
    * fix: adjust button size
    
    * ToogleSwitch
    
    * Удаление файлов из Dropdown компонента
    
    * clean old version
    
    * fix stories
    
    * fix format
    
    * fix format and lint
    
    ---------
    
    Co-authored-by: Mkkkash <marckovakatrin@yandex.ru>
    Co-authored-by: Andrew Nim <131737840+daydreamonly@users.noreply.github.com>
    Co-authored-by: Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>
    Co-authored-by: Mikhail-Zhilkin <mikhailzhilkin17@yandex.ru>
    Co-authored-by: Mikhail Zhilkin <mikhailzhilkin@yandex.ru>
    Co-authored-by: Andrey Krivosheev <122694146+Lukumka@users.noreply.github.com>

--- src/shared/ui/Dropdown/DropdownBase/DropdownBase.stories.tsx
+++ src/shared/ui/Dropdown/DropdownBase/DropdownBase.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { DropdownBaseUI } from './DropdownBase';
 
 const meta: Meta<typeof DropdownBaseUI> = {
-  title: 'Shared/UI/DropdownBaseUI',
+	title: 'Shared/UI/DropdownBaseUI',
 	component: DropdownBaseUI,
 	tags: ['autodocs'],
 	parameters: {
@@ -16,32 +16,32 @@ export default meta;
 type Story = StoryObj<typeof DropdownBaseUI>;
 
 export const Default: Story = {
-  render: (args) => {
-    const [selectedOption, setSelectedOption] = useState<string | undefined>();
-    const [isOpen, setIsOpen] = useState(false);
+	render: (args) => {
+		const [selectedOption, setSelectedOption] = useState<string | undefined>();
+		const [isOpen, setIsOpen] = useState(false);
 
-    return (
-      <div style={{ width: 436, padding: 20 }}>
-        <DropdownBaseUI
-          {...args}
-          selectedOption={selectedOption}
-          isOpen={isOpen}
-          onToggle={() => setIsOpen((prev) => !prev)}
-          onSelect={(value) => {
-            setSelectedOption(value);
-            setIsOpen(false);
-          }}
-        />
-      </div>
-    );
-  },
-  args: {
-    label: 'Пол',
-    placeholder: 'Не указан',
-    options: [ 
-	{ value: '1', text: 'Не указан' },
-	{ value: '2', text: 'Женский' },
-	{ value: '3', text: 'Мужской' },
-	],
-  },
+		return (
+			<div style={{ width: 436, padding: 20 }}>
+				<DropdownBaseUI
+					{...args}
+					selectedOption={selectedOption}
+					isOpen={isOpen}
+					onToggle={() => setIsOpen((prev) => !prev)}
+					onSelect={(value) => {
+						setSelectedOption(value);
+						setIsOpen(false);
+					}}
+				/>
+			</div>
+		);
+	},
+	args: {
+		label: 'Пол',
+		placeholder: 'Не указан',
+		options: [
+			{ value: '1', text: 'Не указан' },
+			{ value: '2', text: 'Женский' },
+			{ value: '3', text: 'Мужской' },
+		],
+	},
 };
