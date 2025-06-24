e36e77e256fed004e8107ead1996ae190fee4893
Andrew Nim <131737840+daydreamonly@users.noreply.github.com>
2025-06-24 13:16:50 +0300

    feature/checkbox-category
    * feat: add CheckboxCategory component files
    
    * feat: implement CheckboxCategory component with styles and stories
    
    * feat: update CheckboxCategoryUI component styles and structure
    
    * feat: implement CheckboxCategoryUI click handling and styles

---
+++ src/shared/ui/CheckboxCategory/CheckboxCategory.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta } from "@storybook/react-vite";
+import { CheckboxCategoryUI } from "./CheckboxCategory";
+import { useState } from "react";
+
+const meta = {
+  title: 'Shared/UI/CheckboxCategory',
+  component: CheckboxCategoryUI,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof CheckboxCategoryUI>;
+
+export default meta;
+
+export const CheckboxCategoryDefault = {
+  render: () => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    return (
+      <div>
+        <CheckboxCategoryUI isChecked={isChecked} onClick={() => setIsChecked(!isChecked)} />
+      </div>
+    )
+  }
+}
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,5 +3,6 @@ export * from './ToggleSwitch';
 export * from './VisibilityButton';
 export * from './Button';
 export * from './LikeButton';
+export * from './CheckboxCategory';
 export * from './Dropdown';
 export * from './CheckboxSubcategory';
