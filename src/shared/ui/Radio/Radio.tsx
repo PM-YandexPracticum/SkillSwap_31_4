f65757b2ca51e7f5fc81ee3ec440b8259a002815
Mkkkash <marckovakatrin@yandex.ru>
2025-06-25 09:33:54 +0300

    fix margin radio

--- src/shared/ui/Radio/Radio.tsx
+++ src/shared/ui/Radio/Radio.tsx
@@ -25,3 +25,31 @@ export const Radio = ({ options, value, onChange, name }: TRadioUIProps) => (
 		})}
 	</div>
 );
+
+/* примерное использование в компоненте фильтры
+const optionsGender: [
+    {value: 'not', label: 'Без разницы'},
+    {value: 'men', label: 'Мужской'},
+    {value: 'women', label: 'Женский'},
+]
+const skills: [
+    {value: 'not', label: 'навык 1'},
+    {value: 'men', label: 'навык 2'},
+    {value: 'women', label: 'навык 3'},
+]
+<h1>Пол Автора</h1>
+<Radio 
+name={gender}
+options={optionsGender}
+value={selectedValue} // Передаём текущее значение
+onChange={handleRadioChange} - Передаём обработчик изменения
+/>
+ 
+ остальной код
+
+<Radio
+name={skills}
+options={optionsSkills}
+value={selectedValue} // Передаём текущее значение
+onChange={handleRadioChange} - Передаём обработчик изменения
+/> */
