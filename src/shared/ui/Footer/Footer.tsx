f8e87218d58761f5cf5665e6fcea800f1fe95a6d
Mikhail Zhilkin <mikhailzhilkin@yandex.ru>
2025-06-26 18:54:18 +0300

    fix: lint fix

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -1,73 +1,71 @@
 import { Logo } from '../Logo';
 import styles from './Footer.module.scss';
 
-export const Footer = () => {
-	return (
-		<footer className={styles.footer}>
-			<Logo />
-			<ul className={styles.footerLinkContainer}>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						О проекте
-					</a>
-				</li>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						Контакты
-					</a>
-				</li>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						Политика конфиденциальности
-					</a>
-				</li>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						Все навыки
-					</a>
-				</li>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						Блог
-					</a>
-				</li>
-				<li>
-					<a
-						href='#'
-						style={{
-							textDecoration: 'none',
-							color: 'var(--color-text-primary)',
-						}}>
-						Пользовательское соглашение
-					</a>
-				</li>
-			</ul>
-			<p className={styles.copyright}>SkillSwap - 2025</p>
-		</footer>
-	);
-};
+export const Footer = () => (
+	<footer className={styles.footer}>
+		<Logo />
+		<ul className={styles.footerLinkContainer}>
+			<li>
+				<a
+					href='#1'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					О проекте
+				</a>
+			</li>
+			<li>
+				<a
+					href='#2'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					Контакты
+				</a>
+			</li>
+			<li>
+				<a
+					href='#3'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					Политика конфиденциальности
+				</a>
+			</li>
+			<li>
+				<a
+					href='#4'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					Все навыки
+				</a>
+			</li>
+			<li>
+				<a
+					href='#5'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					Блог
+				</a>
+			</li>
+			<li>
+				<a
+					href='#6'
+					style={{
+						textDecoration: 'none',
+						color: 'var(--color-text-primary)',
+					}}>
+					Пользовательское соглашение
+				</a>
+			</li>
+		</ul>
+		<p className={styles.copyright}>SkillSwap - 2025</p>
+	</footer>
+);
