da93e84bcf730d4444e7576968684c2d5ac72a46
Andrew Nim <131737840+daydreamonly@users.noreply.github.com>
2025-06-28 14:35:53 +0300

    feature/SuccessModal
    * feat: add SuccessModal component with styles and stories
    
    * fix: update SuccessModal styles and change 'p' to 'h2'

---
+++ src/shared/ui/SuccessModal/SuccessModal.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta } from '@storybook/react-vite';
+import { SuccessModalUI } from './SuccessModal';
+
+const meta = {
+	title: 'Shared/UI/SuccessModalUI',
+	component: SuccessModalUI,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
+} satisfies Meta<typeof SuccessModalUI>;
+
+export default meta;
+
+export const SuccessModalDefault = {
+	render: () => (
+		<div
+			style={{
+				width: '800px',
+				height: '800px',
+				backgroundColor: '#8E8E8E99',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
+			<SuccessModalUI />
+		</div>
+	),
+};
