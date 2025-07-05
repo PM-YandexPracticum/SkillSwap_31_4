da1bd371acccb04f730784fc44a6916b00a3e6c4
daydreamer <nimandrey22@gmail.com>
2025-07-05 07:16:11 +0300

    Add Redux Toolkit and React-Redux integration with store configuration

---
+++ src/services/store.ts
@@ -0,0 +1,22 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import {
+	type TypedUseSelectorHook,
+	useDispatch as dispatchHook,
+	useSelector as selectorHook,
+} from 'react-redux';
+
+export const rootReducer = combineSlices();
+
+const store = configureStore({
+	reducer: rootReducer,
+	devTools: process.env.NODE_ENV !== 'production',
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch: () => AppDispatch = () => dispatchHook();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
+export default store;
