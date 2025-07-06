fc5b8ebcfd6a7c5220d0ea3f98ccaf04cb1112f1
andykms <160009032+andykms@users.noreply.github.com>
2025-07-06 19:18:01 +0300

    feature/widgets: SkillCardsList
    * feat: create SkillCardsList component
    
    * refactor: divide component SkillCardsList on two elements
    feat: add infinite scroll component
    feat: install infinite scroll component
    
    * refactor: delete prop to type of button in SkillCardsListDemo component
    
    ---------
    
    Co-authored-by: Andrey Isaev <andrey.isaev.54.ru@gmail.com>
    Co-authored-by: Alexandr Kotov <88854598+Alexandr-Kotov@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -81,6 +81,7 @@ module.exports = {
 		'jsx-a11y/no-static-element-interactions': 'off',
 		'jsx-a11y/click-events-have-key-events': 'off',
 		'react/button-has-type': 'off',
+		'react/jsx-props-no-spreading': 'off',
 		'react/jsx-no-useless-fragment': 'off',
 		'no-underscore-dangle': ['error', { allow: ['_id'] }],
 	},
