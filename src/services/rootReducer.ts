20ba78f057d4304ce03cc4878a300e5bd7a6ac7e
Mkkkash <marckovakatrin@yandex.ru>
2025-07-09 23:32:19 +0300

    create: notificationsSlice

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,4 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { userSliceReducer } from '../features/user/userSlice';
+import { notificationsSliceReducer } from '../features/notificationsSlice/notificationsSlice';
 
-export const rootReducer = combineReducers({ user: userSliceReducer });
+export const rootReducer = combineReducers({
+	user: userSliceReducer,
+	notifications: notificationsSliceReducer,
+});
