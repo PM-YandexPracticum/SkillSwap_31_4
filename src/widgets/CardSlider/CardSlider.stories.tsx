d8a5ce5bebc89d76d505a8f64c89379f74d6f63d
daydreamer <nimandrey22@gmail.com>
2025-07-10 11:27:44 +0300

    Update CardSlider story

--- src/widgets/CardSlider/CardSlider.stories.tsx
+++ src/widgets/CardSlider/CardSlider.stories.tsx
@@ -1,20 +1,62 @@
 import type { Meta } from '@storybook/react-vite';
 import { MemoryRouter } from 'react-router-dom';
-import { CardSlider } from './CardSlider';
+import { useState } from 'react';
+import { Provider } from 'react-redux';
+import { mockUsers } from './mockData';
+import { CardSliderUI } from './CardSlider';
+import type { SwiperArrowsProps } from '../../shared/ui/SwiperArrows/type';
+import store from '../../services/store';
 
 const meta = {
 	title: 'Widgets/CardSlider',
-	component: CardSlider,
+	component: CardSliderUI,
 	tags: ['autodocs'],
 	parameters: {
 		layout: 'centered',
 	},
-} satisfies Meta<typeof CardSlider>;
+	decorators: [
+		(Story) => (
+			<Provider store={store}>
+				<MemoryRouter>
+					<Story />
+				</MemoryRouter>
+			</Provider>
+		),
+	],
+} satisfies Meta<typeof CardSliderUI>;
 
 export default meta;
 
-export const CardSliderDefault = () => (
-	<MemoryRouter>
-		<CardSlider />
-	</MemoryRouter>
-);
+export const CardSliderDefault = () => {
+	const [sliderIndex, setSliderIndex] = useState(0);
+	const visibleCount = 4;
+
+	const visibleUsers = mockUsers.slice(sliderIndex, sliderIndex + visibleCount);
+
+	const maxIndex = Math.max(0, mockUsers.length - visibleCount);
+
+	const handlePrev = () => {
+		setSliderIndex((prev) =>
+			prev - 1 < 0 ? mockUsers.length - visibleCount : prev - 1
+		);
+	};
+
+	const handleNext = () => {
+		setSliderIndex((prev) =>
+			prev + 1 > mockUsers.length - visibleCount ? 0 : prev + 1
+		);
+	};
+
+	const mockSwipeProps: SwiperArrowsProps = {
+		onNext: handleNext,
+		onPrev: handlePrev,
+		disabledPrev: sliderIndex === 0,
+		disabledNext: sliderIndex >= maxIndex,
+	};
+
+	return (
+		<div>
+			<CardSliderUI users={visibleUsers} swipeProps={mockSwipeProps} />
+		</div>
+	);
+};
